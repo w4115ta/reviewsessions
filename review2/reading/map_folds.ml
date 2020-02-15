064f99cfa165473db672f8cdbfbbfe56e6236358
Amanda Liu <al3623@columbia.edu>
2020-02-15 16:29:19 -0500

    Add another fold example with option

--- review2/reading/map_folds.ml
+++ review2/reading/map_folds.ml
@@ -9,4 +9,10 @@ let m = [[1;0;1;0;1];
          [1010;101;101;1;12]]
 
 let num_elements m = fold_left (+) 0 (map length m)
+
 let matrix_sum m = fold_left (+) 0 (map (fold_left (+) 0) m)
+
+let find_max m = 
+    let rec lmax = fold_left (fun m x -> max m (Some x)) None in
+    let rec maybemax = fold_left (fun m x -> max m x) None in
+    maybemax (map lmax m)
